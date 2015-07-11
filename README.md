a3c97bdafccfa00f1775a596b27c07f18667faf2
Daniel Pryor <pryorda@users.noreply.github.com>
2015-07-11 12:47:13 -0600

    Updating readme and initial snapshot-vm.py

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # vmware-scripts
+To use these scripts you will need run sudo pip install pyVmomi
+###### snapshot-vm.py
+This script is will create, remove, or revert a snapshot.
+`
+user@host:~/vmware-scripts$ ./snapshot-vm.py -h
+Usage: snapshot-vm.py -h
+
+Options:
+  -h, --help            show this help message and exit
+  -v VCENTER, --vcenter=VCENTER
+                        -v vcenter-ip or dns name
+  -p PASSWORD, --password=PASSWORD
+                        -p vcenter-password
+  -u USER, --user=USER  -u vcenter-user
+  -m VM, --vm=VM        -m virtual-machine
+  -o OPERATION, --operation=OPERATION
+                        -o create, remove, or revert
+  -n SNAPNAME, --name=SNAPNAME
+                        -n snapshot-name
+  -d DESCRIPTION, --description=DESCRIPTION
+                        -d description
+user@host:~/vmware-scripts$ nano README.md
+`
---
+++ snapshot-vm.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+'''
+Copyright 2013-2014 Reubenur Rahman
+All Rights Reserved
+@author: reuben.13@gmail.com
+Rewrote with parameterization by pryorda
+'''
+
+import atexit
+#import argparse
+import sys
+import time
+import optparse
+import getpass
+
+from optparse import OptionParser
+from pyVmomi import vim, vmodl
+from pyVim import connect
+from pyVim.connect import Disconnect, SmartConnect, GetSi
+
+
+def get_obj(content, vimtype, name):
+    """
+     Get the vsphere object associated with a given text name
+    """
+    obj = None
+    container = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
+    for c in container.view:
+        if c.name == name:
+            obj = c
+            break
+    return obj
+
+def wait_for_task(task, raiseOnError=True, si=None, pc=None):
+    if si is None:
+        si = GetSi()
+
+    if pc is None:
+        sc = si.RetrieveContent()
+        pc = sc.propertyCollector
+
+    # First create the object specification as the task object.
+    objspec = vmodl.Query.PropertyCollector.ObjectSpec()
+    objspec.SetObj(task)
+
+    # Next, create the property specification as the state.
+    propspec = vmodl.Query.PropertyCollector.PropertySpec()
+    propspec.SetType(vim.Task);
+    propspec.SetPathSet(["info.state"]);
+    propspec.SetAll(True)
+
+    # Create a filter spec with the specified object and property spec.
+    filterspec = vmodl.Query.PropertyCollector.FilterSpec()
+    filterspec.SetObjectSet([objspec])
+    filterspec.SetPropSet([propspec])
+
+    # Create the filter
+    filter = pc.CreateFilter(filterspec, True)
+
+    # Loop looking for updates till the state moves to a completed state.
+    taskName = task.GetInfo().GetName()
+    update = pc.WaitForUpdates(None)
+    state = task.GetInfo().GetState()
+    while state != vim.TaskInfo.State.success and \
+            state != vim.TaskInfo.State.error:
+        if (state == 'running') and (taskName.info.name != "Destroy"):
+            # check to see if VM needs to ask a question, thow exception
+            vm = task.GetInfo().GetEntity()
+            if vm is not None and isinstance(vm, vim.VirtualMachine):
+                qst = vm.GetRuntime().GetQuestion()
+            if qst is not None:
+                raise Exception("Task blocked, User Intervention required")
+
+    update = pc.WaitForUpdates(update.GetVersion())
+    state = task.GetInfo().GetState()
+
+    filter.Destroy()
+    if state == "error" and raiseOnError:
+        raise task.GetInfo().GetError()
+
+    return state
+
+
+def invoke_and_track(func, *args, **kw):
+    try :
+        task = func(*args, **kw)
+        wait_for_task(task)
+    except:
+        raise
+
+def main():
+
+    usage = "usage: %prog -h"
+    parser = OptionParser(usage=usage)
+
+    # Add -s option
+    parser.add_option("-v", "--vcenter", dest='vcenter',
+                  help="-v vcenter-ip or dns name")
+    parser.add_option("-p", "--password", dest='password',
+                  help="-p vcenter-password")
+    parser.add_option("-u", "--user", dest='user',
+                  help="-u vcenter-user")
+    parser.add_option("-m", "--vm", dest='vm',
+                  help="-m virtual-machine")
+    parser.add_option("-o", "--operation", dest='operation',
+                  help="-o create, remove, or revert")
+    parser.add_option("-n", "--name", dest='snapname',
+                  help="-n snapshot-name")
+    parser.add_option("-d", "--description", dest='description',
+                  help="-d description")
+
+
+
+    # Parse Args
+    (options, args) = parser.parse_args()
+
+    if not options.vm or not options.vcenter or not options.password or not options.user or not options.vm or not options.operation or not options.snapname or not options.description:
+        parser.error("Refer to usage for help")
+        sys.exit()
+
+
+    inputs = {'vcenter_ip': options.vcenter,
+              'vcenter_password': options.password,
+              'vcenter_user': options.user,
+              'vm_name' : options.vm,
+              #create, remove or revert
+              'operation' : options.operation,
+              'snapshot_name' : options.snapname,
+              'description': options.description,
+              }
+
+
+    try:
+        si = None
+        try:
+            print "Trying to connect to VCENTER SERVER . . ."
+            #si = connect.Connect(args.host, int(args.port), args.user, args.password, service="hostd")
+            si = connect.Connect(inputs['vcenter_ip'], 443, inputs['vcenter_user'], inputs['vcenter_password'])
+        except IOError, e:
+            pass
+            atexit.register(Disconnect, si)
+
+        print "Connected to VCENTER SERVER !"
+
+        content = si.RetrieveContent()
+
+        operation = inputs['operation']
+
+        vm_name = inputs['vm_name']
+        snapshot_name = inputs['snapshot_name']
+
+        vm = get_obj(content, [vim.VirtualMachine], vm_name)
+
+        if operation == 'create':
+
+            # Read about dumpMemory : http://pubs.vmware.com/vi3/sdk/ReferenceGuide/vim.VirtualMachine.html#createSnapshot
+            dumpMemory = False
+            quiesce = False
+            description = inputs['description']
+            print "Creating snapshot %s for vm: %s" % (snapshot_name, vm_name)
+            invoke_and_track(vm.CreateSnapshot(snapshot_name, description, dumpMemory, quiesce))
+
+        elif operation == 'remove':
+            snapshots = vm.snapshot.rootSnapshotList
+
+            for snapshot in snapshots:
+                if snapshot_name == snapshot.name:
+                    snap_obj = snapshot.snapshot
+                    print "Removing snapshot ", snap_obj
+                    invoke_and_track(snap_obj.RemoveSnapshot_Task(True))
+                else:
+                    print "Couldn't find any snapshots"
+
+    except vmodl.MethodFault, e:
+        print "Caught vmodl fault: %s" % e.msg
+        return 1
+    except Exception, e:
+        if str(e).startswith("'vim.Task'"):
+            return 1
+        print "Caught exception: %s" % str(e)
+        return 1
+
+# Start program
+if __name__ == "__main__":
+    main()
+
